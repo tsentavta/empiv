a4e9ececa97fcd56848c2d5a4b7b951201107aaf
iTsenty <etentavtaru@mail.ru>
2023-08-01 19:42:02 +0400

    first

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import './App.css';
-import {BrowserRouter, HashRouter, useLocation} from "react-router-dom";
 import {PublicRoutes} from "./components/routes";
 import {Footer} from "./components/Footer/Footer";
 import NavBar from "./components/NavBar/NavBar";
+import {Main} from "./pages/Main";
+import {Route} from "react-router-dom";
+import {SkinEffect} from "./pages/SkinEffect";
 
 function App() {
     return (
         <div className="App">
-            <HashRouter>
-                <NavBar/>
-                <PublicRoutes/>
+            <NavBar/>
+            <Route exact path={'/'} component={Main}/>
+            <Route exact path={'/SkinEffect'} component={SkinEffect}/>
+
+                {/*<PublicRoutes/>*/}
                 <Footer/>
 
-            </HashRouter>
+
         </div>
     );
 }
