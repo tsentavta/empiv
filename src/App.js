345a4b034dcfe284bef34dde64ecb557b8d505dd
iTsenty <etentavtaru@mail.ru>
2023-08-01 20:00:27 +0400

    firsting

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import {PublicRoutes} from "./components/routes";
 import {Footer} from "./components/Footer/Footer";
 import NavBar from "./components/NavBar/NavBar";
 import {Main} from "./pages/Main";
-import {Route} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import {SkinEffect} from "./pages/SkinEffect";
 
 function App() {
     return (
         <div className="App">
             <NavBar/>
-            non
+            <Routes>
+                <Route exact path={'/'} component={Main}/>
+                <Route exact path={'/SkinEffect'} component={SkinEffect}/>
+
+            </Routes>
             <Route exact path={'/'} component={Main}/>
             <Route exact path={'/SkinEffect'} component={SkinEffect}/>
 
